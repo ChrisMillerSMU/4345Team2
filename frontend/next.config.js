a3dbcb2ed3fb663d28e896a82eaacd0617d18ff4
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-10-10 14:20:11 -0500

    Tsx Conversion

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+const withFonts = require('next-fonts');
 
-module.exports = nextConfig
+(module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
+  },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: !process.env.ENABLE_PWA && process.env.NODE_ENV === 'development',
+  },
+})),
+  withFonts({
+    enableSvg: true,
+    webpack(config, options) {
+      return config;
+    },
+  });
