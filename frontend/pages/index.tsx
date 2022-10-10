71b645c1d7fa90733e3a4208e875d0ebe2f494ea
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-10-10 15:53:01 -0500

    Nav Routing and Auth page [IN PROGRESS]

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import NavLink from '../components/NavLink'
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Sign In Page: <a href="/auth">Click Here</a>
         </h1>
 
         <p className={styles.description}>
